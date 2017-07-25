fbf5113edc1a5aaad1b19419b2c07ee9d6385688
kesavanraja <kesav.kraja@gmail.com>
2017-07-25 09:45:33 +0530

    Create kesavan5.c

---
+++ kesavan5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the number");
+    scanf("%d",&a);
+    if(number % 2 == 0)
+    {
+    printf("%d is even number",a);
+    else
+    printf("%d is odd number",a);
+    }
+    getch();
+}
