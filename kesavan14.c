39c5ee6e8b88e8440fda36dcfca7561c22a2746e
kesavanraja <kesav.kraja@gmail.com>
2017-07-25 10:32:12 +0530

    Update kesavan14.c

--- kesavan14.c
+++ kesavan14.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 void main()
 {
- int n, i, sum = 0;
+ int n,i,a=0;
  printf("Enter the positive number");
  scanf("%d",&n);
  for(i=1;i<=n;++i)
  {
- sum += i;
+ a+=i;
  }
- printf("Sum = %d",sum);
+ printf("Sum=%d",sum);
  getch();
 }
