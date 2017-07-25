428b7446b62e90ed9658b1464f19f4deab54bfbe
kesavanraja <kesav.kraja@gmail.com>
2017-07-25 11:51:56 +0530

    Create palin.c

---
+++ palin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+int n, reversedInteger = 0, remainder, originalInteger;
+printf("Enter the number");
+scanf("%d",&n);
+originalInteger = n;
+while(n!=0)
+{
+remainder=n%10;
+reversedInteger = reversedInteger*10 + remainder;
+n/=10;
+}
+if (originalInteger == reversedInteger)
+{
+printf("%d is a palindrome",originalInteger);
+else
+printf("%d is not a palindrome",originalInteger);
+}
+getch();
+}
